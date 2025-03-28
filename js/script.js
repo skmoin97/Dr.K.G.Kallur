848e21c655031b6cb57382ecf1db6c69749ab682
skmoin97 <skmoinuddin1997@gmail.com>
2025-03-28 09:28:00 +0530

    Add files via upload
    All files are uploaded successfully.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let mainListDiv = document.getElementById("navmenulist"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("fa-x");
+    
+};
+
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";   
+    } 
+  });
+}
